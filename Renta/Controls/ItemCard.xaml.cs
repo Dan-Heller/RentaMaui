700ac158a5b1f7dcec64eec711501b630aefabb3
David Davidson <ddavid8888@outlook.com>
2022-03-29 18:31:19 +0300

    added like button for itemcard

--- Renta/Controls/ItemCard.xaml.cs
+++ Renta/Controls/ItemCard.xaml.cs
@@ -3,6 +3,7 @@ namespace Renta.Controls;
 public partial class ItemCard : ContentView
 {
 
+	public bool HeartButtonClicked { get; set; }
 
 	public ItemCard()
 	{
@@ -12,5 +13,18 @@ public partial class ItemCard : ContentView
     private async void ItemCard_Tapped(object sender, EventArgs e)
     {
 		await Shell.Current.GoToAsync($"{nameof(ItemPage)}");
+	}
+
+    private void onCardHeartTapped(object sender, EventArgs e)
+    {
+		if(HeartButtonClicked == false)
+        {
+			HeartButton.Source = "hollowhearticon.png";
+		}
+        else
+        {
+			HeartButton.Source = "fullhearticon.png";
+		}
+		HeartButtonClicked = !HeartButtonClicked;
 	}
 }
