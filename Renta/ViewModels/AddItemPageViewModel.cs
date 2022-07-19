9b6efdc212b7bad72f9f0af623f3f3ce651f201a
Dan Heller <danheller@outlook.co.il>
2022-07-19 18:19:45 +0300

    after vd update, logo added to first page

--- Renta/ViewModels/AddItemPageViewModel.cs
+++ Renta/ViewModels/AddItemPageViewModel.cs
@@ -8,7 +8,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using static Android.Renderscripts.ScriptGroup;
+//using static Android.Renderscripts.ScriptGroup;
 
 
 namespace Renta.ViewModels
--- Renta/ViewModels/EditItemPageViewMode.cs
+++ Renta/ViewModels/EditItemPageViewMode.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using Android.Locations;
+//using Android.Locations;
 using Microsoft.Maui.Storage;
 
 namespace Renta.ViewModels
