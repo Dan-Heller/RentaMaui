ab5b85a554c416eb823681c86eefb45b6fa0900b
Dan Heller <danheller@outlook.co.il>
2022-08-15 14:02:18 +0300

    simple chat test

--- Renta/Views/MessagesPage.xaml.cs
+++ Renta/Views/MessagesPage.xaml.cs
@@ -1,9 +1,33 @@
+using Microsoft.AspNetCore.SignalR.Client;
+
 namespace Renta;
 
 public partial class MessagesPage : ContentPage
 {
+	private readonly HubConnection _connection;
+
 	public MessagesPage()
 	{
 		InitializeComponent();
+
+		_connection = new HubConnectionBuilder().WithUrl("https://474b-85-65-247-184.eu.ngrok.io/chat").Build();
+		_connection.On<string>("MessageReceived", (message) =>
+		{
+			chatMessages.Text += $"{Environment.NewLine}{message}";
+		} );
+
+		Task.Run(() =>
+		{
+			Dispatcher.Dispatch(async () =>
+			await _connection.StartAsync());
+		});
+
+    }
+
+	private async void SendButton_Clicked(object sender, EventArgs e)
+	{
+		await _connection.InvokeCoreAsync("SendMessage", args: new[] { myChatMessage.Text });
+
+		myChatMessage.Text = string.Empty;
 	}
 }
