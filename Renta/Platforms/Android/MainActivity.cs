0968ac3b3db3acef85da95f84ed3a08803db38dc
david <david@aptenobytes.com>
2022-08-27 18:26:33 +0300

    something

--- Renta/Platforms/Android/MainActivity.cs
+++ Renta/Platforms/Android/MainActivity.cs
@@ -1,10 +1,17 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Plugin.FirebasePushNotification;
 
 namespace Renta;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        
+        FirebasePushNotificationManager.ProcessIntent(this, Intent);
+    }
 }
